5a3279e469dfff65490451d6f9747d998041bd25
Furquan712 <smfurquan7.12@gmail.com>
2023-08-17 17:30:25 +0530

    quote card created from quote.json file

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
+import QuoteCardList from '../components/quotes/QuoteCardList'
 
 const Quote = () => {
   return (
-    <div>Quote</div>
+     <>
+     <Navbar />
+      <QuoteCardList />
+     <Footer />
+     </>
   )
 }
 
